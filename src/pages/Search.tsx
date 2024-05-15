2d0886bfb28a170682cbe6d8575e84623bf57089
Contradictive88 <lanceorville5@gmail.com>
2024-05-15 22:22:59 +0800

    finished first section of search

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,33 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, ReactNode } from 'react';
 import { Button } from "../components/ui/button";
-import { BiBuilding } from "react-icons/bi";
+import { BiBuilding, BiUser, BiLocationPlus, BiCalendarAlt } from "react-icons/bi";
+import { TbZoom } from "react-icons/tb";
 
 const Search: React.FC = () => {
   const [tripType, setTripType] = useState<string>("one-way");
 
+  const companyOptions = [
+    { value: "", label: "Select a bus terminal" },
+    { value: "araceli-bus-terminal", label: "Araceli Bus Terminal" },
+    { value: "cubao-bus-terminal", label: "Cubao Bus Terminal" },
+    { value: "buendia-bus-terminal", label: "Buendia Bus Terminal" },
+    { value: "pasay-bus-terminal", label: "Pasay Bus Terminal" },
+    { value: "balintawak-bus-terminal", label: "Balintawak Bus Terminal" }
+  ];
+
   return (
-    <div className='grid grid-cols-2 gap-4 px-20 py-8'>
-     <TripButtons tripType={tripType} setTripType={setTripType} />
-     <div className='border-2 rounded-sm flex items-center p-3 h-12'>
-         <BiBuilding size={40} />
-         <p className='pl-2 pr-5'>Company:</p>
-         <select className='text-black w-full h-auto bg-white dark:bg-zinc-950'>
-          <option value="">Select a bus terminal</option>
-          <option value="araceli-bus-terminal">Araceli Bus Terminal</option>
-          <option value="cubao-bus-terminal">Cubao Bus Terminal</option>
-          <option value="buendia-bus-terminal">Buendia Bus Terminal</option>
-          <option value="pasay-bus-terminal">Pasay Bus Terminal</option>
-          <option value="balintawak-bus-terminal">Balintawak Bus Terminal</option>
-         </select>
-        </div>
+    <div className='px-20 py-8'>
+     <div className='grid grid-cols-2 gap-4'>
+      <TripButtons tripType={tripType} setTripType={setTripType} />
+      <SelectDropdown icon={<BiBuilding size={40}/>} selectTitle={'Company'} options={companyOptions} />
+      <InputField icon={<BiUser size={40}/>} selectTitle={'Passenger'} />
+      <DestinationRangeField firstIcon={<BiLocationPlus size={40}/>} secondIcon={<BiLocationPlus size={40}/>}  />
+      <DestinationDateRangeField firstIcon={<BiCalendarAlt size={40}/>} secondIcon={<BiCalendarAlt size={40}/>} />
+     </div>
+     <div className='grid grid-cols-1 py-4'>
+        <Button className='h-12 text-md flex items-center justify-center gap-2'>
+            <TbZoom size={25}/>
+            <span>Search</span>
+        </Button>
+     </div>
     </div>
   );
 }
 
-const TripButtons: React.FC<{ tripType: string; 
-    setTripType: React.Dispatch<React.SetStateAction<string>>; 
-    }> = ({ tripType, setTripType }) => {
-        
+const TripButtons: React.FC<{ tripType: string; setTripType: React.Dispatch<React.SetStateAction<string>>; }> = ({ tripType, setTripType }) => {
     return ( 
         <>
             <Button 
@@ -47,5 +54,75 @@ const TripButtons: React.FC<{ tripType: string;
         </>
     );
 }
+
+const SelectDropdown: React.FC<{ icon: ReactNode; selectTitle: string; options: { value: string; label: string; }[]}> = ({ icon, selectTitle, options }) => (
+    <div className='border-2 rounded-sm flex items-center py-3 pl-3 h-12'>
+        {icon}
+        <p className='pl-2 pr-5'>{selectTitle + ':'}</p>
+        <select className='w-full h-11 bg-white text-zinc-950 dark:bg-zinc-950 dark:text-white'>
+            {options.map((option, index) => (
+                <option key={index} value={option.value}>{option.label}</option>
+            ))}
+        </select>
+    </div>
+);
+
+const InputField: React.FC<{ icon: React.ReactNode; selectTitle: string }> = ({ icon, selectTitle }) => (
+    <div className='border-2 rounded-sm flex items-center py-3 pl-3 h-12'>
+        {icon}
+        <p className='pl-2 pr-5'>{selectTitle + ':'}</p>
+        <input 
+            className='w-full h-11 bg-white text-zinc-950 dark:bg-zinc-950 dark:text-white' 
+            type="number" 
+            pattern="[0-9]*"
+        />    
+    </div>
+);
+
+const DestinationRangeField: React.FC<{ firstIcon: React.ReactNode; secondIcon: React.ReactNode; }> = ({ firstIcon, secondIcon }) => (
+    <div className='border-2 rounded-sm grid grid-cols-2 h-12'>
+        <div className='flex items-center border-r-2 rounded-sm pl-2'>
+            {firstIcon}
+            <p className='pl-2 pr-5'>From:</p>
+            <input 
+                className='w-full h-11 bg-white text-zinc-950 dark:bg-zinc-950 dark:text-white' 
+                type='text' 
+                placeholder='Origin'
+            /> 
+        </div>
+        <div className='flex items-center pl-2'>
+            {secondIcon}
+            <p className='pl-2 pr-5'>To:</p>
+            <input 
+                className='w-full h-11 bg-white text-zinc-950 dark:bg-zinc-950 dark:text-white' 
+                type='text' 
+                placeholder='Destination'
+            /> 
+        </div>
+    </div>
+);
+
+const DestinationDateRangeField: React.FC<{ firstIcon: React.ReactNode; secondIcon: React.ReactNode; }> = ({ firstIcon, secondIcon }) => (
+    <div className='border-2 rounded-sm grid grid-cols-2 h-12'>
+        <div className='flex items-center border-r-2 rounded-sm pl-2'>
+            {firstIcon}
+            <p className='pl-2 pr-5'>Departure:</p>
+            <input 
+                className='w-full h-11 bg-white text-zinc-950 dark:bg-zinc-950 dark:text-white' 
+                type='date' 
+                placeholder='Origin'
+            /> 
+        </div>
+        <div className='flex items-center pl-2'>
+            {secondIcon}
+            <p className='pl-2 pr-5'>Return:</p>
+            <input 
+                className='w-full h-11 bg-white text-zinc-950 dark:bg-zinc-950 dark:text-white' 
+                type='date' 
+                placeholder='Destination'
+            /> 
+        </div>
+    </div>
+);
 
 export default Search;
