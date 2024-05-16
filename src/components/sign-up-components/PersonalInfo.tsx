6e9584200d42b4c8f5bc7397b16457e902703beb
Contradictive88 <lanceorville5@gmail.com>
2024-05-16 16:31:59 +0800

    Sign up page complete

--- src/components/sign-up-components/PersonalInfo.tsx
+++ src/components/sign-up-components/PersonalInfo.tsx
@@ -1,6 +1,20 @@
 import { Button } from "../ui/button";
 
-const PersonalInfo = () => {
+interface PersonalInfoProps {
+    firstName: string;
+    setFirstName: React.Dispatch<React.SetStateAction<string>>;
+    middleName: string;
+    setMiddleName: React.Dispatch<React.SetStateAction<string>>;
+    lastName: string;
+    setLastName: React.Dispatch<React.SetStateAction<string>>;
+    suffix: string;
+    setSuffix: React.Dispatch<React.SetStateAction<string>>;
+    gender: string;
+    setGender: React.Dispatch<React.SetStateAction<string>>;
+    handleNext: () => void;
+}
+
+const PersonalInfo: React.FC<PersonalInfoProps> = ({ firstName, setFirstName, middleName, setMiddleName, lastName, setLastName, suffix, setSuffix, gender, setGender, handleNext }) => {
     return (<div className="grid grid-cols-2 bg-gray-800 p-8 rounded-md max-w-screen-xl mx-auto">
         <div className="grid grid-rows-3 text-right px-8">
             <span className="text-5xl font-bold mb-2">
@@ -12,7 +26,7 @@ const PersonalInfo = () => {
                 Create an account to track your reservations, updates on schedules and special offers. 
                 Start your journey by securing your bus ride today and enjoy a hassle-free travel experience.
             </span>
-            <div className="flex justify-end gap-2 mt-auto">
+            <div className="flex justify-end gap-4 mt-auto">
             <span>Terms</span>
             <span>Privacy</span>
             <span>Contact</span>
@@ -24,6 +38,8 @@ const PersonalInfo = () => {
                 <input 
                     className='w-11/12 h-11 bg-transparent' 
                     type='text' 
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
                 /> 
             </div>
             <div className='flex items-center border-2 rounded-sm border-white mb-3'>
@@ -31,6 +47,8 @@ const PersonalInfo = () => {
                 <input 
                     className='w-11/12 h-11 bg-transparent' 
                     type='text' 
+                    value={middleName}
+                    onChange={(e) => setMiddleName(e.target.value)}
                 /> 
             </div>
             <div className='flex items-center border-2 rounded-sm border-white mb-3'>
@@ -38,11 +56,13 @@ const PersonalInfo = () => {
                 <input 
                     className='w-11/12 h-11 bg-transparent' 
                     type='text' 
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
                 /> 
             </div>
             <div className='flex items-center border-2 rounded-sm border-white mb-3'>
                 <p className='pl-6 font-bold w-2/12'>Suffix:</p>
-                <select className='w-11/12 h-11 bg-transparent'>
+                <select className='w-11/12 h-11 bg-transparent' value={suffix} onChange={(e) => setSuffix(e.target.value)}>
                     <option className="text-black" value="">Select Your Suffix</option>
                     <option className="text-black" value="sr">Sr</option>
                     <option className="text-black" value="jr">Jr.</option>
@@ -51,7 +71,7 @@ const PersonalInfo = () => {
             </div>
             <div className='flex items-center border-2 rounded-sm border-white mb-3'>
                 <p className='pl-6 font-bold w-2/12'>Gender:</p>
-                <select className='w-11/12 h-11 bg-transparent'>
+                <select className='w-11/12 h-11 bg-transparent' value={gender} onChange={(e) => setGender(e.target.value)}>
                     <option className="text-black" value="">Select Your Gender</option>
                     <option className="text-black" value="male">Male</option>
                     <option className="text-black" value="female">Female</option>
@@ -59,7 +79,7 @@ const PersonalInfo = () => {
                 </select>
             </div>
             <div className='grid grid-cols-1 py-3'>
-             <Button className='h-12 font-bold'>Next</Button>
+             <Button className='h-12 font-bold' onClick={handleNext}>Next</Button>
             </div>
             <div className="flex justify-center gap-1">
             <div>
